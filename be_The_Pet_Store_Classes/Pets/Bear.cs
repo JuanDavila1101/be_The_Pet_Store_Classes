97ae7030c95cda274bc0b4eccbfc50103c922209
Juan Davila <juandavil1101@gmail.com>
2021-07-21 16:51:34 -0500

    Theses are the Class changes for Tuesday 7-20-21

---
+++ be_The_Pet_Store_Classes/Pets/Bear.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be_The_Pet_Store_Classes.Pets
+{
+  class Bear
+  {
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int NumberOffFishEaten { get; private set; }
+    public string EducatioLevel { get; set; } = "GED";
+    public bool LovesHoney { get; }
+    public Tiger BestTigerFriend { get; set; }
+
+    // writing constructor ctor and 2xtab
+    public Bear(string name, string type)
+    {
+      Name = name;
+      Type = type;
+      EducatioLevel = "GED";
+
+      if (name == "Winnie the Pooh")
+      {
+        LovesHoney = true;
+      }
+      else if (name == "Yogi")
+      {
+        EducatioLevel = "Above Average";
+      }
+      else
+      {
+        EducatioLevel = "High School";
+      }
+
+    }
+
+    public Bear(string name, string type, string educationLevel) : this(name, type)
+    {
+
+    }
+
+    public void EatFish(int howMany)
+    {
+      if (howMany <= 0) return;
+      NumberOffFishEaten += howMany;
+    }
+
+    public void HangOutWithBestFriend()
+    {
+      Console.WriteLine($"{Name}'s best friend is {BestTigerFriend}\n");
+    }
+
+
+  }
+}
---
+++ be_The_Pet_Store_Classes/Pets/Tiger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be_The_Pet_Store_Classes.Pets
+{
+  class Tiger
+  {
+    // { get; set; } 
+    public string Name { get; set; }
+    public string Size { get; set; }
+    public int NumberOfKills { get; set; }
+    public short Speed { get; set; }
+
+    // Fields 
+    // Fields are things that I want to keep track of in the class 
+    // but I don't want to expose external to the class
+    // Fields start with _ and then camelCase
+    // explicit private for: private int _numberOfBites;
+    int _numberOfBites;
+
+    public void Bite(string handlerName)
+    {
+      _numberOfBites++;
+      Console.WriteLine($"\n{Name} the tiger just bit {handlerName}. \nThat's the {_numberOfBites} time.\n");
+    }
+
+
+  }
+
+}
